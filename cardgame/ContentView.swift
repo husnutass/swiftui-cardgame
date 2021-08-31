6687846b959d77a5f44ef95446509649f7706953
husnutass <husnutas@outlook.com>
2021-08-31 20:04:01 +0300

    Initial Commit

---
+++ cardgame/ContentView.swift
@@ -0,0 +1,97 @@
+//
+//  ContentView.swift
+//  cardgame
+//
+//  Created by Hüsnü Taş on 6.04.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var playerCard = "card5"
+    @State var cpuCard = "card9"
+    @State var playerScore = 0
+    @State var cpuScore = 0
+    
+    var body: some View {
+        ZStack {
+            Image("background")
+                .ignoresSafeArea()
+            VStack {
+                Image("logo")
+                    .resizable()
+                    .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fit/*@END_MENU_TOKEN@*/)
+                    .padding(.top, 50)
+                    .fixedSize(horizontal: /*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/, vertical: /*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
+                
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(cpuCard)
+                    Spacer()
+                }
+                
+                Spacer()
+                
+                Button(action: {
+                    
+                    let playerRand = Int.random(in: 2...14)
+                    let cpuRand = Int.random(in: 2...14)
+                    
+                    playerCard = "card" + String(playerRand)
+                    cpuCard = "card" + String(cpuRand)
+                    
+                    if playerRand > cpuRand {
+                        playerScore += 1
+                    } else if cpuRand > playerRand {
+                        cpuScore += 1
+                    }
+                    
+                }, label:{
+                    Image("dealbutton")
+                })
+                
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    VStack {
+                        Text("Player")
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10.0)
+                        Text(String(playerScore))
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    }
+                    Spacer()
+                    VStack {
+                        Text("CPU")
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10.0)
+                        Text(String(cpuScore))
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    }
+                    Spacer()
+                }
+                Spacer()
+                
+            }
+            
+        }
+        
+    }
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ cardgame/cardgameApp.swift
@@ -0,0 +1,17 @@
+//
+//  cardgameApp.swift
+//  cardgame
+//
+//  Created by Hüsnü Taş on 6.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct cardgameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
